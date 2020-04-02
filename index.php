beef36fc117c38b781be03926ec452e646b6db6d
Saqlain Abbas <saqlainabbas478@gmail.com>
2020-04-02 22:34:27 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+
+session_start();
+
+require 'database.php';
+
+if( isset($_SESSION['user_id']) ){
+
+	$records = $conn->prepare('SELECT id,email,password FROM users WHERE id = :id');
+	$records->bindParam(':id', $_SESSION['user_id']);
+	$records->execute();
+	$results = $records->fetch(PDO::FETCH_ASSOC);
+
+	$user = NULL;
+
+	if( count($results) > 0){
+		$user = $results;
+	}
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body  {
+  background-image: url("background.jpg");
+  background-color: #cccccc;
+  cursor: url("cursor.png"),progress !important;
+}
+</style>
+	<title>Full On Chill</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+	<?php if( !empty($user) ): ?>
+
+    <?php
+include 'includes/func.php';
+
+$title  = 'FullOnChill'; //Edit Homepage Title
+
+include 'includes/config.php';
+echo'<h1>All You Need Is Here</h1>';
+echo '<h2>Select Category</h2>';
+include'category.php';
+echo '<h2><a>Facebook.com/PakIdiots</a> | <a>Full On Chill</a></h2>';
+?>
+
+	<?php else: ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Below</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+	<script data-ad-client="ca-pub-6184791442727848" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</head>
+<body>
+
+	<div class="header">
+		<a href="/">Full On Chill</a>
+	</div>
+
+	<?php if(!empty($message)): ?>
+		<p><?= $message ?></p>
+	<?php endif; ?>
+
+ <h1>Login</h1>
+	<span> <a href="register.php"></a></span>
+
+	<form action="login.php" method="POST">
+
+		<input type="text" placeholder="Enter your email" name="email">
+		<input type="password" placeholder="and password" name="password">
+
+		<input type="submit">
+
+	</form>
+
+</body>
+</html>
+
+	<?php endif; ?>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,64 @@
+<?php
+
+session_start();
+
+if( isset($_SESSION['user_id']) ){
+	header("Location: /");
+}
+
+require 'database.php';
+
+if(!empty($_POST['email']) && !empty($_POST['password'])):
+	
+	$records = $conn->prepare('SELECT id,email,password FROM users WHERE email = :email');
+	$records->bindParam(':email', $_POST['email']);
+	$records->execute();
+	$results = $records->fetch(PDO::FETCH_ASSOC);
+
+	$message = '';
+
+	if(count($results) > 0 && password_verify($_POST['password'], $results['password']) ){
+
+		$_SESSION['user_id'] = $results['id'];
+		header("Location: /");
+
+	} else {
+		$message = 'Sorry, those credentials do not match';
+	}
+
+endif;
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Below</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+	<script data-ad-client="ca-pub-6184791442727848" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</head>
+<body>
+
+	<div class="header">
+		<a href="/">Full On Chill</a>
+	</div>
+
+	<?php if(!empty($message)): ?>
+		<p><?= $message ?></p>
+	<?php endif; ?>
+
+	<h1>Login</h1>
+	<span> <a href="register.php"></a></span>
+
+	<form action="login.php" method="POST">
+		
+		<input type="text" placeholder="Enter your email" name="email">
+		<input type="password" placeholder="and password" name="password">
+
+		<input type="submit">
+
+	</form>
+
+</body>
+</html>
