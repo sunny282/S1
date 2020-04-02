13c965fa2d568c6a4f123337385f7fcd9a415bbb
Saqlain Abbas <saqlainabbas478@gmail.com>
2020-04-02 23:01:59 +0400

    Update category.php

--- category.php
+++ category.php
@@ -1,9 +1,9 @@
 <?php 
 echo '
-<div style="padding-bottom:10px;padding-top:11px;background:;"><a href="/latest"> <b><font color="white" size="5">New Videos</font><font color="lime" size="5"> </font></b></a></div></center>
-<div style="padding-bottom:10px;padding-top:11px;background:;"><a href="/scandel"> <b><font color="white" size="5">Scandel</font><font color="lime" size="5"> </font></b></a></div></center>
-<div style="padding-bottom:10px;padding-top:11px;background:;"><a href="/lesbian"> <b><font color="white" size="5">Lesbian</font><font color="lime" size="5"> </font></b></a></div></center>
-<div style="padding-bottom:10px;padding-top:11px;background:;"><a href="/logout.php"><img src="/images/logout.png" height="25" width="20"> <b><font color="red" size="5">Click Here To Logout</font><font color="lime" size="5"> </font></b></a></div></center>
+<div style="padding-bottom:10px;padding-top:11px;background:;"><a href="https://www.xvideos.com/"> <b><font color="white" size="5">New Videos</font><font color="lime" size="5"> </font></b></a></div></center>
+<div style="padding-bottom:10px;padding-top:11px;background:;"><a href="https://www.ndtv.com/topic/sex-scandal"> <b><font color="white" size="5">Scandel</font><font color="lime" size="5"> </font></b></a></div></center>
+<div style="padding-bottom:10px;padding-top:11px;background:;"><a href="https://www.youtube.com/"> <b><font color="white" size="5">Lesbian</font><font color="lime" size="5"> </font></b></a></div></center>
+<div style="padding-bottom:10px;padding-top:11px;background:;"><a href="facebook.com/pakidiots"><img src="/images/logout.png" height="25" width="20"> <b><font color="red" size="5">Click Here To Logout</font><font color="lime" size="5"> </font></b></a></div></center>
 
 '; 
 ?>
