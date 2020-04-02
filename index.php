6b6e93c449d55765c2d8d723b43758f85982e5ba
Saqlain Abbas <saqlainabbas478@gmail.com>
2020-04-02 22:46:41 +0400

    Update index.php

--- index.php
+++ index.php
@@ -27,43 +27,5 @@ echo '<h2>Select Category</h2>';
 include'category.php';
 echo '<h2><a>Facebook.com/PakIdiots</a> | <a>Full On Chill</a></h2>';
 ?>
-
-	<?php else: ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Login Below</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
-	<script data-ad-client="ca-pub-6184791442727848" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-</head>
-<body>
-
-	<div class="header">
-		<a href="/">Full On Chill</a>
-	</div>
-
-	<?php if(!empty($message)): ?>
-		<p><?= $message ?></p>
-	<?php endif; ?>
-
- <h1>Login</h1>
-	<span> <a href="register.php"></a></span>
-
-	<form action="login.php" method="POST">
-
-		<input type="text" placeholder="Enter your email" name="email">
-		<input type="password" placeholder="and password" name="password">
-
-		<input type="submit">
-
-	</form>
-
-</body>
-</html>
-
-	<?php endif; ?>
-
 </body>
 </html>
