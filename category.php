9653f3f60dd0f9f0ffb862cc75ec1433267a52ac
Saqlain Abbas <saqlainabbas478@gmail.com>
2020-04-02 23:04:03 +0400

    Update category.php

--- category.php
+++ category.php
@@ -3,7 +3,7 @@ echo '
 <div style="padding-bottom:10px;padding-top:11px;background:;"><a href="https://www.xvideos.com/"> <b><font color="white" size="5">New Videos</font><font color="lime" size="5"> </font></b></a></div></center>
 <div style="padding-bottom:10px;padding-top:11px;background:;"><a href="https://www.ndtv.com/topic/sex-scandal"> <b><font color="white" size="5">Scandel</font><font color="lime" size="5"> </font></b></a></div></center>
 <div style="padding-bottom:10px;padding-top:11px;background:;"><a href="https://www.youtube.com/"> <b><font color="white" size="5">Lesbian</font><font color="lime" size="5"> </font></b></a></div></center>
-<div style="padding-bottom:10px;padding-top:11px;background:;"><a href="facebook.com/pakidiots"><img src="/images/logout.png" height="25" width="20"> <b><font color="red" size="5">Click Here To Logout</font><font color="lime" size="5"> </font></b></a></div></center>
+<div style="padding-bottom:10px;padding-top:11px;background:;"><a href="https://www.facebook.com/pakidiots"><img src="/images/logout.png" height="25" width="20"> <b><font color="red" size="5">Click Here To Logout</font><font color="lime" size="5"> </font></b></a></div></center>
 
 '; 
 ?>
